a99011bf58d3ebb2ff4f05ec60be928401fe154c
mccright <matt.mccright@gmail.com>
2020-12-21 06:56:41 -0600

    Spelling correction

--- trylogging/__about__.py
+++ trylogging/__about__.py
@@ -1,7 +1,7 @@
 __title__ = 'trylogging'
 #__package_name__ = 'not-a-package'
 __version__ = '0.0.1'
-__description__ = "Just an example of using the Python logging module to remind me about usiing it."
+__description__ = "Just an example of using the Python logging module to remind me about using it."
 __email__ = "notNeededForThisExample@null.net"
 __author__ = 'Matt McCright'
 __github__ = 'https://github.com/mccright/PythonLoggingReminders'
