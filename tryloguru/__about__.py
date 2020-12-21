196c8dc3e276c7edd878550f705d42d665a6f68c
mccright <matt.mccright@gmail.com>
2020-12-21 06:58:06 -0600

    Corrected repo link

--- tryloguru/__about__.py
+++ tryloguru/__about__.py
@@ -1,10 +1,10 @@
 __title__ = 'tryloguru'
 #__package_name__ = 'not-a-package'
 __version__ = '0.0.1'
-__description__ = "Just an example of using the LoGuru module to remind me about usiing it."
+__description__ = "Just an example of using the LoGuru module to remind me about using it."
 __email__ = "notNeededForThisExample@null.net"
 __author__ = 'Matt McCright'
-__github__ = 'https://github.com/mccright/PythonLoggingReminders'
+__github__ = 'https://github.com/mccright/PythonLoggingExamples'
 #__pypi__ = 'https://pypi.org/project/ --> not-a-package'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 - Matt McCright'
