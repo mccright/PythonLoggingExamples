341cd045f6092f83cb83fb7c62e051cdb52f1da6
mccright <matt.mccright@gmail.com>
2021-02-25 13:42:13 -0600

    Initial commit

---
+++ requestsDebugging/devClient.py
@@ -0,0 +1,33 @@
+import http
+import logging
+import requests
+
+# This is well explained by Ben Hoey at:
+# https://bhoey.com/blog/better-debug-logging-for-the-python-requests-library/
+# This is his excellent approach to requests debug logging.  Thank you Ben.
+# It is especially valuable when attempting to deal with a poorly documented or otherwise opaque API.
+# I have it here just as a reminder to me... 
+ 
+# Set up logging to a file
+logging.basicConfig(filename="devClient.log", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+ 
+# Turn on global debugging for the HTTPConnection class
+http.client.HTTPConnection.debuglevel = 1
+ 
+# Redirect print to the debug logger
+def debugLog(*args):
+    logger.debug(" ".join(args))
+http.client.print = debugLog
+ 
+ 
+# Assumes you have something listening for tests
+url = "http://localhost:8080/test"
+ 
+logger.info("START HTTP GET Test")
+# Test HTTP GET
+resp = requests.get(url)
+ 
+logger.info("START HTTP POST Test")
+# Test HTTP POST
+resp = requests.post(url, data='POST Testing data')
